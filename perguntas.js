0ce3e49dc7031d1a1d9ad64820ec88e1fefac04c
2dduds63 <mariaeduardasantanaidid@gmail.com>
2024-11-26 07:57:48 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'serie US',
+    'Quem é a principal vítima de Joe na primeira temporada?',
+    'Guinevere Beck, a escritora com quem ele se envolve.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'filme Us',
+    'Qual o plost do filme Us?',
+    'Adelaide, foi trocada por sua sósia quando criança.'
 )
 
 criaCartao(
